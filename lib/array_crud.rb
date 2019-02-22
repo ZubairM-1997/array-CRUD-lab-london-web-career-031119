0e781829792658694bd09a2b96b0218120fa1759
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 19:03:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,52 @@
 def create_an_empty_array
+  array = []
   
 end
 
 def create_an_array
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array.Push("ford")
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array.Unshift("ford")
+  
   
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array.Pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array.Shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array[3]
   
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
+  array[-1]
   
 end
