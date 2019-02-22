28908758b4be2c508af56d6cc4e21e50ac878e1e
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 19:04:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,31 +20,23 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   array.Pop
-  
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   array.Shift
-  
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   array[3]
   
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   array[0]
-  
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["volswagen", "bmw" , "mercedes", "rolls royce"]
   array[-1]
   
 end
